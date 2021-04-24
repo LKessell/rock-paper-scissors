cf0cebde376d722658c1d6c32f79a8750a720c2e
Lauren Kessell <lkessell1@gmail.com>
2021-04-24 14:13:17 -0400

    Instantiate Game, add query selectors

---
+++ main.js
@@ -0,0 +1,17 @@
+var game = new Game();
+
+var classicBtn = document.getElementById('classicBtn');
+var challengeBtn = document.getElementById('challengeBtn');
+var mainView = document.getElementById('mainView');
+var choiceView = document.getElementById('choiceView');
+var resultsView = document.getElementById('resultsView');
+var humanWinCount = document.getElementById('humanWinCount');
+var computerWinCount = document.getElementById('computerWinCount');
+
+
+
+// game.computer.takeTurn();
+// game.human.takeTurn('rock');
+//
+// console.log(`You chose: ${game.human.currentChoice}`);
+// console.log(`Computer chose: ${game.computer.currentChoice}`);
