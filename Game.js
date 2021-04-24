649a2bd6e8c45e0e8eec59968574d4417ecac6e0
Lauren Kessell <lkessell1@gmail.com>
2021-04-24 12:11:15 -0400

    Add checkForWin method

--- Game.js
+++ Game.js
@@ -3,13 +3,24 @@ class Game {
     this.player1 = new Player('Human', '😄');
     this.player2 = new Player('Computer', '💻')
     this.type = 'classic';
-    this.currentTurn;
     this.choices = ['rock', 'paper', 'scissors'];
   }
+
   checkForWin() {
-    // Check player choice vs computer
-    // win, lose, draw cases?
+    var humanWin =
+      player1.currentChoice === 'rock' && player2.currentChoice === 'scissors' || player1.currentChoice === 'paper' && player2.currentChoice === 'rock' || player1.currentChoice === 'scissors' && player2.currentChoice === 'paper';
+
+    if (humanWin) {
+      console.log('You won!');
+      player1.wins++;
+    } else if (player1.currentChoice === player2.currentChoice) {
+      console.log('It\'s a draw!');
+    } else {
+      console.log('Computer wins!');
+      player2.wins++;
+    }
   }
+
   resetBoard() {
   }
 }
